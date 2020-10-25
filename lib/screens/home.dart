50cce7d279d35de050b47456523a77b97a82df4d
tmbruce <travis.m.bruce@gmail.com>
2020-10-24 18:34:44 -0700

    Remove boilerplate, work on homescreen

---
+++ lib/screens/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static String id = 'HomeScreen';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('AlgoViz'),
+      ),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
